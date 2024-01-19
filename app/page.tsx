ead9425696c51ffea826777ff259feb3299f0813
ashik-75 <ashikex49@gmail.com>
2024-01-19 12:24:19 +0600

    fix some chnage

--- app/page.tsx
+++ app/page.tsx
@@ -1,20 +1,18 @@
 import React from "react";
 import BlurImage from "@/components/blur-image";
 import { projects } from "@/data/projects";
-import Link from "next/link";
 
 const Page = () => {
   return (
-    <div className="min-h-full bg-orange-50/50">
-      <div className="mx-auto h-full max-w-5xl space-y-10 bg-white p-10">
+    <div className="min-h-full bg-purple-50/50">
+      <div className="mx-auto h-full max-w-5xl space-y-10 bg-white p-5 md:p-16">
         <div>
           <div className="space-y-2">
             <h1 className="text-3xl font-black">Sweet Rana</h1>
             <p>Frontend Software Developer</p>
-            <p className="max-w-md">
+            <p className="max-w-xl">
               Frontend Engineer focused on building products with extra
-              attention to detail and loves to work with React, Next js and
-              Typescript.
+              attention to detail.
             </p>
           </div>
         </div>
@@ -22,19 +20,18 @@ const Page = () => {
         <div>
           <h1 className="text-2xl font-bold">About</h1>
           <p className="max-w-xl">
-            As a Full Stack Engineer, I have successfully taken multiple
-            products from 0 to 1.I lead teams effectively, ensuring an
-            environment where people can do their best work. Currently, I work
-            mostly with TypeScript, React, Next.js, and Tailwind. I have over 2
-            years of experience in working locally and remotely with companies
-            all around the world.
+            As a Frontend Engineer, I have successfully taken multiple products
+            from 0 to 1 and love working with functional and data-driven
+            applications. I primarily work with TypeScript, React, Redux, and
+            Tailwind. I have two years of experience working locally and
+            remotely with companies worldwide.
           </p>
         </div>
 
         <div className="space-y-5">
           <h1 className="text-2xl font-bold">Experience</h1>
           <div className="space-y-5">
-            <div className="max-w-md">
+            <div className="max-w-xl">
               <div className="text-sm">2022 - 2023</div>
 
               <div>
@@ -43,15 +40,16 @@ const Page = () => {
                 </h1>
                 <p className="mb-2 text-sm">GigaTech Solution</p>
                 <p>
-                  Implemented new features, led squad, worked on improving the
-                  way developers ship the code, started migration from Emotion
-                  to Tailwind CSS and more. Technologies: React, TypeScript,
-                  GraphQL
+                  Develop a collection of tools for developers, which will serve
+                  as a one-stop-shop for building applications. As a frontend
+                  developer working with React and TypeScript, I was responsible
+                  for managing the frontend functionality and design of the
+                  application. I also tested the app with Vite.
                 </p>
               </div>
             </div>
 
-            <div className="max-w-md">
+            <div className="max-w-xl">
               <div className="text-sm">2021 - 2022</div>
 
               <div>
@@ -59,10 +57,12 @@ const Page = () => {
                 <p className="mb-2 text-sm">DevsNest LLC </p>
 
                 <p>
-                  Implemented new features, led squad, worked on improving the
-                  way developers ship the code, started migration from Emotion
-                  to Tailwind CSS and more. Technologies: React, TypeScript,
-                  GraphQL
+                  DevsNest created a Shopify app for the Shopify App Store. My
+                  responsibility was to manage the frontend functionality and
+                  frontend design of the application.To obtain data and develop
+                  features, I collaborated with the backend development team.
+                  Since DevsNest has both PHP and Node.js developer teams, I
+                  worked closely with them to develop Shopify apps.
                 </p>
               </div>
             </div>
@@ -72,18 +72,19 @@ const Page = () => {
         <div className="space-y-5">
           <h1 className="text-2xl font-bold">Skiills</h1>
 
-          <div className="flex flex-wrap gap-5">
+          <div className="flex flex-wrap gap-2 md:gap-4">
             {[
+              "Javascript",
+              "Typescript",
               "React",
               "Next.js",
-              "Typescript",
-              "Javascript",
-              "React-Query",
+              "React-Query/RTK Query/SWR",
               "Zustand/Redux",
+              "Cypress/Jest/Vitest",
             ].map((tech) => (
               <span
                 key={Date.now()}
-                className="rounded border px-4 py-1 text-sm"
+                className="rounded-3xl border px-3 py-1 text-xs"
               >
                 {tech}
               </span>
@@ -91,7 +92,7 @@ const Page = () => {
           </div>
         </div>
 
-        <div className="space-y-5 pb-20">
+        <div className="space-y-5">
           <h1 className="text-2xl font-bold">Projects</h1>
           <div className="grid grid-cols-1 gap-5 md:grid-cols-3">
             {projects.map((project) => (
--- data/projects.tsx
+++ data/projects.tsx
@@ -1,45 +1,49 @@
 export const projects = [
   {
     id: 1,
-    url: "/image/clay.png",
-    title: "Modern Landing Page",
-    path: "https://npm-trend.vercel.app/",
+    url: "/image/binarytree.png",
+    path: "https://binarytree.dev/",
+    title: "Collection of tools for developer",
     description:
-      "An amazing landing page for your business to landing your site",
+      "Array of Developer Productivity Tools Designed to Help You Save Time",
   },
   {
     id: 2,
-    url: "/image/yt-1.png",
-    title: "Add Free Youtube From 0",
+    url: "/image/npm-trends.png",
     path: "https://npm-trend.vercel.app/",
-    description: "Playwing with Youtube Api from rapid api, also show comments",
+    title: "Statictics for Npm package Downloads",
+    description:
+      "Display an interactive chart depicting the number of downloads for an npm package, presented in an engaging and interactive manner.",
   },
   {
     id: 3,
+    url: "/image/clay.png",
+    title: "Modern Landing Page",
+    path: "https://clay-react.vercel.app/",
+    description:
+      "An amazing landing page for your business to enhance your site's landing experience",
+  },
+  {
+    id: 4,
+    url: "/image/yt-1.png",
+    title: "Create youtube with Rapid Api",
+    path: "https://react-yt-amber.vercel.app/",
+    description:
+      "Create a YouTube platform with the assistance of the RapidAPI for watching ad-free videos.",
+  },
+  {
+    id: 5,
     url: "/image/intercom.png",
-    path: "https://npm-trend.vercel.app/",
-    title: "Add Free Youtube From 0",
-    description: "Playwing with Youtube Api from rapid api, also show comments",
+    path: "https://intercom-coral.vercel.app/",
+    title: "Daashboard for music app",
+    description: "Interactive dashboard for music application",
   },
   {
     id: 4,
     url: "/image/ecom.jpeg",
-    path: "https://npm-trend.vercel.app/",
+    path: "https://next-sanity-groq.vercel.app/",
     title: "Headless Ecommer with Stripe",
-    description: "Playwing with Youtube Api from rapid api, also show comments",
-  },
-  {
-    id: 5,
-    url: "/image/binarytree.png",
-    path: "https://npm-trend.vercel.app/",
-    title: "Array of Developer Productivity for devs",
-    description: "Playwing with Youtube Api from rapid api, also show comments",
-  },
-  {
-    id: 5,
-    url: "/image/npm-trends.png",
-    path: "https://npm-trend.vercel.app/",
-    title: "Array of Developer Productivity for devs",
-    description: "Playwing with Youtube Api from rapid api, also show comments",
+    description:
+      "A fully functional headless e-commerce website with Sanity and Stripe as the payment gateway ",
   },
 ];
