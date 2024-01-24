fe7feb10c0b0267d2be9816e84f96df293944765
ashik-75 <ashikex49@gmail.com>
2024-01-24 22:47:42 +0600

    update info

--- app/page.tsx
+++ app/page.tsx
@@ -10,10 +10,6 @@ const Page = () => {
           <div className="space-y-2">
             <h1 className="text-3xl font-black">Sweet Rana</h1>
             <p>Frontend Software Engineer</p>
-            <p className="max-w-xl">
-              Frontend Engineer focused on building products with extra
-              attention to detail.
-            </p>
 
             <div className="space-y-2">
               <div className="flex items-center gap-3">
